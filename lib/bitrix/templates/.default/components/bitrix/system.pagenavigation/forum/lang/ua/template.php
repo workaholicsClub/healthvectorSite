deb06b1e9de2450513aa2210a7f2752c5a92eecf
Alexander Pavlov <ap@tsys.pw>
2018-01-04 19:00:16 +0300

    Виртуалка на docker compose

--- lib/bitrix/templates/.default/components/bitrix/system.pagenavigation/forum/lang/ua/template.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS["nav_all"] = "Всі";
-$MESS["nav_paged"] = "За стор.";
-$MESS["nav_prev"] = "Поперед.";
-$MESS["nav_next"] = "Наст.";
-$MESS["pages"] = "Сторінки:";
-?>
--- lib/bitrixadapter/mvc/model.php
+++
@@ -1,9 +0,0 @@
-<?php
-abstract class BAModel {
-  protected $arParams;
-  public function __construct($arParams)
-  {
-    $this->arParams = $arParams;
-  }
-  
-}
